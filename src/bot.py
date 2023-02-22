b324652463434da7805f02a12e23a67ac4947b5b
pixcelo <bodomu@gmail.com>
2023-02-23 03:53:36 +0900

    update

--- src/bot.py
+++ src/bot.py
@@ -11,6 +11,9 @@ from loguru import logger
 test_net = 'https://api-testnet.bybit.com'
 main_net = 'https://api.bybit.com'
 
+# settings
+qty = 0.001
+
 # apis
 apis = {
     "bybit_testnet": ["KAA3Gq8p8hqjFpROui", "FnZ8EdaOcSTsxtLtLLmgkB1EpRE43SouR3RS"],
@@ -24,30 +27,68 @@ async def main():
         while True:
             # REST APIデータ並列リクエスト
             resps = await asyncio.gather(
-                client.get('/v2/public/kline/list', params={
+                client.get('public/linear/kline', params={
                     'symbol': 'BTCUSD', 'interval': 1, 'from': int(time.time()) - 3600
                 }),
-                client.get('/v2/private/order', params={'symbol': 'BTCUSD'}),
-                client.get('/v2/private/position/list', params={'symbol': 'BTCUSD'}),
+                # https://bybit-exchange.github.io/docs-legacy/futuresV2/linear/#t-getactive
+                client.get('private/linear/order/list', params={'symbol': 'BTCUSD'}),
+                client.get('private/linear/position/list', params={'symbol': 'BTCUSD'}),
             )
             kline, order, position = await asyncio.gather(*[r.json() for r in resps])
 
             # シグナル計算
             # klineからdfを作る ATRを計算、シグなる判定、ロジック
+            df = pd.DataFrame(kline['result'],
+                    columns=[
+                    'open_time',
+                    # 'open',
+                    'high',
+                    'low',
+                    'close',
+                    # 'volume'
+                    ])
+            
+            # データ型を変換
+            df = df.astype({
+                'high': 'float',
+                'low': 'float',
+                'close': 'float'
+            })
+
+            # オーダーを確認（指値を出したが、約定していないものをチェック）
+            # 画面で言うとアクティブな注文、買いの注文、売りの注
+            # アクティブな注文があれば、約定までN回待機する
+            #　オーダーがなければ指値を出す
+
+
+            # ポジションを確認（約定済みのオーダーをチェック、損切りの判断）
+            # ポジションがなければ指値を出す
+
+            cl = df['close'].values
+            hi = df['high'].values
+            lo = df['low'].values
+
+            # ATR
+            atr_period = 14
+            atr = talib.ATR(hi, lo, cl, timeperiod=atr_period)
+
+            # 閾値
+            threshold = 0.5
+
+            # 指値の価格
+            buy_price = cl - (atr * threshold)
+            sell_price = cl + (atr * threshold)
+
+            
+            if True:
+                data = await market(client, 'Buy', qty)
+                # data = await limit(client, 'Buy', qty, buy_price[atr_period])
+                #     logger.info(data)
+                #　ログをとる関数
+                # 通知する関数
 
             # 注文する関数
             # ポジションを管理する関数
-            # 通知する関数
-            #　ログをとる関数
-            cond = True #'Whether to execute...'
-            side = 'Buy'
-            qty = 0.001
-            price = 22000
-
-            # オーダー執行
-            if cond:
-                data = await limit(client, side, qty, price)
-                logger.info(data)
 
             # 待機(60秒)
             await asyncio.sleep(60.0)
@@ -75,6 +116,15 @@ async def limit(client, side, qty, price):
         'time_in_force': 'GoodTillCancel',
         'reduce_only': False,
         'close_on_trigger': False
+    })
+    data = await res.json()
+    return data
+
+async def cancel(client):
+    res = await client.post('/private/linear/order/cancel', data={
+        'symbol': 'BTCUSDT',
+        # 'order_id': '',
+        # 'order_link_id': ''
     })
     data = await res.json()
     return data
